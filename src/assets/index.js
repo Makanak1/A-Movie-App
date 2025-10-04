f2db31deeb7d011e414e1341d8ff70799713a80e
makanak1 <oyinibrahim07@gmail.com>
2025-10-04 13:48:19 +0100

    movie-app

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from '../App.jsx';
+import reportWebVitals from '../reportWebVitals';
+
+
+ReactDOM.render(<App />, document.getElementById('root')
+);
+
+reportWebVitals();
